6d2174d04356a9e27d1bcd512a3d7a5e59fc47d2
Gamermertcan <115120358+Gamermertcan@users.noreply.github.com>
2024-05-22 23:56:06 +0300

    Create dksl1.js

---
+++ JavaScript/dksl1.js
@@ -0,0 +1,47 @@
+function get(dksl = example_dksl, take = 'main') {
+  if (dksl.startsWith('__dksl__')) {
+    let startIdx = dksl.indexOf('_' + take + '_') + take.length + 1;
+    let endIdx = dksl.indexOf('_-' + take + '_');
+
+    let mainBlock = dksl.slice(startIdx, endIdx).trim();
+    let subBlocks = [];
+    let currentBlock = '';
+    let inBlock = false;
+
+    for (let char of mainBlock) {
+      if (char === '_' && !inBlock) {
+        inBlock = true;
+      } else if (char === '_' && inBlock) {
+        inBlock = false;
+        if (currentBlock) {
+          subBlocks.push(currentBlock.trim());
+          currentBlock = '';
+        }
+      } else if (inBlock) {
+        currentBlock += char;
+      }
+    }
+
+    if (currentBlock) {
+      subBlocks.push(currentBlock.trim());
+    }
+
+    return subBlocks.filter(block => block);
+  }
+
+  return null;
+}
+
+let example_dksl = `__dksl__
+_main_
+_data_
+Example
+_-data_
+_data2_
+Example2
+_data3_
+Example3
+_-data3_
+_-data2_
+_-main_`;
+
